2bdc871640f0edaa3d2d36d7324e9aeed10ac386
a6a2f5842 <office@blax.at>
2025-12-03 14:18:38 +0100

    A testbench, BF tests

---
+++ workbench/app/Models/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Workbench\App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Blax\Shop\Traits\HasShoppingCapabilities;
+use Blax\Shop\Traits\HasStripeAccount;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, HasShoppingCapabilities, HasUuids, HasStripeAccount;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
